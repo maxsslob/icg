30bb8d8edcc52f2b1e50ee01a0657cd032e14976
maxsslob <maxsslob@gmail.com>
2016-12-26 18:48:31 +0300

    Services, About, Services Pages, Routes, Heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,20 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './404/404.component';
 
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import { LandingComponent } from './landing/landing.component';
+import { SeoComponent } from './seo/seo.component';
+import { ContextComponent } from './context/context.component';
+import { InstagramComponent } from './instagram/instagram.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
+  { path: 'services/landing', component: LandingComponent },
+  { path: 'services/context', component: ContextComponent },
+  { path: 'services/instagram', component: InstagramComponent },
+  { path: 'services/seo', component: SeoComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: '404', component: NotFoundComponent },
@@ -33,7 +41,11 @@ const appRoutes: Routes = [
     ContactComponent,
     PortfolioComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LandingComponent,
+    SeoComponent,
+    ContextComponent,
+    InstagramComponent
   ],
   imports: [
     BrowserModule,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,6 +13,7 @@ export class ContactComponent implements OnInit {
   lng: number = 27.467358;
   zoom: number = 13;
   scrollwheel: boolean = false;
+  mystyles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dde6e8"},{"visibility":"on"}]}];
 
   ngOnInit() {
   }
